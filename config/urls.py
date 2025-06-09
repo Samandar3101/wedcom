512a910156c78daef4bed13ee000de15f1e55217
yakubjanov004 <yoqubjonov229@gmail.com>
2025-06-09 16:35:24 +0500

    09.06.2025

--- config/urls.py
+++ config/urls.py
@@ -42,12 +42,12 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('CustomerUser.urls')),
-    path('api/tests/', include('Test.urls')),  # Test app URLs
-    path('api/payments/', include('Payment.urls')),  # Payment app URLs
+    path('tests/', include('Test.urls')),  # Test app URLs
+    path('payments/', include('Payment.urls')),  # Payment app URLs
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
